d2be13e3fa6cfc2d6e8bb24f556ba8a433a27a26
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-02-23 18:46:16 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+try:
+	with open("status.md" , "r") as f:
+		text = f.readline()
+		num = [int(x) for x in text.split() if x.isdigit()][0]
+		num += 1
+	
+	if num<= 99999999999999999999999999999999:
+		with open("status.md" , 'w') as f:
+			if num in range(16):
+				img = "![goal](PR.png)"
+			elif num in range(128):
+				img = "![goal](PR.png)"
+			elif num in range(1024):
+				img = "![goal](PR.png)"
+			else:
+				img = "![goal](PR.png)"
+			
+			f.write(f"{num} pull requests merged")
+			f.write("<br>")
+			f.write("Currently:")
+			f.write("<br>")
+			f.write(img)
+except:
+	with open("status.md" , "w") as f:
+		f.write("1 pull request merged")
+		f.write("<br>")
+		f.write("Currently:")
+		f.write("<br>")
+		f.write("![goal](PR.png)")
